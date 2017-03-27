a7138ca11389239b6946d0d76ee93325eb5cef10
Cherniak Vitaliy <chornoy15102001@gmail.com>
2017-03-27 14:44:39 +0300

    first commit

---
+++ src/test/java/org/selenium/demo/GeneralTest.java
@@ -0,0 +1,7 @@
+package org.selenium.demo;
+
+/**
+ * Created by web_systems on 17.03.2017.
+ */
+public class GeneralTest {
+}
---
+++ src/test/java/org/selenium/demo/Test_websystems.java
@@ -0,0 +1,7 @@
+package org.selenium.demo;
+
+/**
+ * Created by web_systems on 15.03.2017.
+ */
+public class Test_websystems {
+}
